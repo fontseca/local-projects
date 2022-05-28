4f19e28fdc9b7589299f4d357c7983c7b9fce561
fontseca <alx.17@outlook.com>
2022-05-28 14:32:48 -0600

    test novel

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 import { Novel } from './projects';
 
-const novel: Novel = new Novel();
+try {
+  const novel: Novel = new Novel();
+  novel.setTitle('Novel');
 
-novel.setTitle('La Caravana Pasa');
-
-const id1 = novel.addChapter('Chapter 1');
-const id2 = novel.addChapter('Chapter 2');
-const id3 = novel.addChapter('Chapter 3');
-const id4 = novel.addChapter('Chapter 4');
-
-// const sc1 = novel.addScene(id4, 'Contenido escena 1');
-// novel.addScene(id4, 'Contenido escena 2');
-// novel.addScene(id4, 'Contenido escena 3');
-
-// novel.addNote(id4, sc1, 'Note');
-
-console.log(novel);
+  console.log(novel);
+} catch (err) {
+  console.error(err);
+}
