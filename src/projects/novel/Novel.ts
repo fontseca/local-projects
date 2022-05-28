67d3298e1ffa81ee517d855e62953e0ce2daf5ff
fontseca <alx.17@outlook.com>
2022-05-28 14:32:09 -0600

    novel methods

--- src/projects/novel/Novel.ts
+++ src/projects/novel/Novel.ts
@@ -1,107 +1,67 @@
-import { Identifier } from '../../common/types';
-import { Tree } from '../../data-structres';
+import { Hash } from '../../common/types';
+import { List } from '../../data-structres';
+import { Chapter, Character } from './classes';
+import Scene from './classes/Scene';
 
-class Character {
-  private name!: string;
-  private overview!: string;
-  private personality!: string;
-
-  public getName(): string {
-    return this.name;
-  }
-
-  public setName(name: string): void {
-    this.name = name;
-  }
-
-  public setOverview(overview: string) {
-    this.overview = overview;
-  }
-
-  public getOverview(): string {
-    return this.overview;
-  }
-
-  public getPersonality(): string {
-    return this.personality;
-  }
-
-  public setPersonality(personality: string): void {
-    this.personality = personality;
-  }
-}
-
-class Novel {
+export default class Novel {
   private title: string = 'Untitled';
   private characters: Array<Character> = new Array<Character>();
-  private chapters: number = 0;
-  private tail: Tree | null = null;
+  private chapters: List<Chapter> = new List<Chapter>();
 
+  /**
+   * Adds a new chapters to the list
+   * @param {string} title The title of the chapter to add
+   * @returns {Hash} The unique Hashs of the chapter
+   */
+  public addChapter(title: string): Hash {
+    const ch: Chapter = new Chapter();
+    ch.setTitle(title);
+    this.chapters.insertAtEnd(ch);
+    return ch.getHash();
+  }
+
+  /**
+   * Adds a new scene to a given chapter
+   * @param {Hash} hash The hash of the chapter to insert the new sece
+   * @param {string} content The content of the new scene
+   * @returns {Hash} The hash of the created scene
+   */
+  public addScene(hash: Hash, content: string): Hash {
+    if (!this.chapters.count())
+      throw 'Error: This novel does not have chapters';
+
+    const sc: Scene = new Scene();
+
+    /* Get the chapter by its hash */
+    const ch = this.chapters.where((c) => c.getHash() === hash);
+
+    sc.appendContent(content);
+    ch?.scenes.insertAtEnd(sc);
+
+    return sc.getHash();
+  }
+
+  /**
+   * Gets an array of all chapters of the novel
+   * @returns {Array<Chapter>} The chapters
+   */
+  public chapterEntries(): Array<Chapter> {
+    return this.chapters.entries();
+  }
+
+  /**
+   * Gets the title of the novel
+   * @returns {string}
+   */
   public getTitle(): string {
     return this.title;
   }
 
+  /**
+   * Updates the title of the novel
+   * @param title New title
+   */
   public setTitle(title: string): void {
     this.title = title;
   }
-
-  public addChapter(title: string): Identifier {
-    const chapter: Tree = new Tree();
-
-    chapter.setTitle(title);
-
-    // If first chapter
-    if (this.tail === null) {
-      this.tail = chapter;
-      this.chapters++;
-      return chapter.getIdentifier();
-    }
-
-    chapter.setParent(this.tail);
-    this.tail = chapter;
-
-    this.chapters++;
-
-    return chapter.getIdentifier();
-  }
-
-  public addScene(
-    chapterIdentifier: Identifier,
-    content: string
-  ): Identifier | null {
-    const scene: Tree = new Tree();
-    const ch: Tree | null = this.searchChapter(chapterIdentifier);
-
-    // Chapter not found
-    if (!ch) return null;
-
-    scene.appendContent(content);
-    scene.setParent(ch);
-    ch.appendChild(scene);
-    return scene.getIdentifier();
-  }
-
-  public searchChapter(identifier: Identifier): Tree | null {
-    let ch: Tree | null = this.tail;
-
-    while (ch !== null) {
-      if (ch.getIdentifier() === identifier) break;
-      ch = ch.getParent();
-    }
-
-    /* Not found */
-    if (!ch) return null;
-
-    return ch;
-  }
-
-  public getChaptersNumber(): number {
-    return this.chapters;
-  }
-
-  public getCharactersNumber(): number {
-    return this.characters.length;
-  }
 }
-
-export default Novel;
