f4b75aa4f2f9e1874adb05e81288eef86dd09d4a
fontseca <alx.17@outlook.com>
2022-05-29 18:02:01 -0600

    export perpetual list

--- src/data-structres/index.ts
+++ src/data-structres/index.ts
@@ -1,4 +1,5 @@
 import List from './list/List';
+import PerpetualList from './perpetual-list/PerpetualList';
 import Tree from './tree/Tree';
 
-export { Tree, List };
+export { Tree, List, PerpetualList };
